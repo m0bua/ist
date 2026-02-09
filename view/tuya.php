43055cba102ba42493029f3e237a4d54f01c1ed3
m0b <git@m0b.in>
2026-02-09 11:59:56 +0000

    CSS fix.

--- view/tuya.php
+++ view/tuya.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= str_replace('_', ' ', $data->dev->get('params.name')) ?> Chart</title>
-    <link rel=stylesheet type="text/css" href="res/tuya.css">
+    <link rel=stylesheet type="text/css" href="res/tuya.css?v1">
     <script src="res/chart.min.js"></script>
 </head>
 
