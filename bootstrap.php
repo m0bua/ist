fe22c508a2d6ae38167c11441c0213cffc147c69
m0b <git@m0b.in>
2026-03-30 07:03:02 +0000

    Timezone.

--- bootstrap.php
+++ bootstrap.php
@@ -1,19 +1,21 @@
 <?php
 
-date_default_timezone_set('Europe/Kyiv');
 defined('ROOT') or define('ROOT', __DIR__);
 defined('IS_WEB') or define('IS_WEB', php_sapi_name() != 'cli');
 require ROOT . '/vendor/autoload.php';
 spl_autoload_register(function ($class) {
-    $file = implode(
-        DIRECTORY_SEPARATOR,
-        array_merge(
-            [ROOT, 'logic'],
-            explode('\\', $class . '.php')
-        )
-    );
+    $file = implode(DIRECTORY_SEPARATOR, array_merge(
+        [ROOT, 'logic'],
+        explode('\\', $class . '.php')
+    ));
     if (file_exists($file)) include $file;
 });
+
+if (is_file('cfg/php.php')) {
+    $cfg = include 'cfg/php.php';
+    if (is_array($cfg)) foreach (include 'cfg/php.php' as $opt => $val)
+        ini_set($opt, $val);
+}
 
 Helpers\DB::config(include 'cfg/db.php');
 if (IS_WEB) (new Auth)->start();
