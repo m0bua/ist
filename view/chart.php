a2422b608b6083220884b1ecd71a8d4269c7750a
m0b <git@m0b.in>
2026-04-06 08:56:34 +0000

    CSS update.

--- view/chart.php
+++ view/chart.php
@@ -12,7 +12,7 @@ $data = Html::getChartData($_GET);
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?= str_replace('_', ' ', $data->dev->get('params.name')) ?> Chart</title>
-  <link rel=stylesheet type="text/css" href="res/chart.css?v=202604031251">
+  <link rel=stylesheet type="text/css" href="res/chart.css?v=202604061156">
 </head>
 
 <body id="body" class="dark">
